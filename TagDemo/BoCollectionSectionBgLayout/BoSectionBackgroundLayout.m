8f4bf67f19c9a41f500f1d5c300bc60327c68447
alienjun <alienjun@126.com>
2015-09-13 12:24:05 +0800

    固定cell 间距。

--- TagDemo/BoCollectionSectionBgLayout/BoSectionBackgroundLayout.m
+++ TagDemo/BoCollectionSectionBgLayout/BoSectionBackgroundLayout.m
@@ -32,7 +32,7 @@ NSString *decorationViewOfKind = @"decorationBgView";
             frame = CGRectMake(0, 0, self.collectionView.frame.size.width, 36);
             if (section == 1) {
                 UICollectionViewLayoutAttributes *headerView = [self.collectionView.collectionViewLayout layoutAttributesForSupplementaryViewOfKind:UICollectionElementKindSectionHeader atIndexPath:[NSIndexPath indexPathForRow:0 inSection:section]];
-                attributes.frame = CGRectMake(0, CGRectGetMaxY(headerView.frame), self.collectionView.frame.size.width, defaultHeight);
+                frame = CGRectMake(0, CGRectGetMaxY(headerView.frame), self.collectionView.frame.size.width, defaultHeight);
             }
         }else{
             UICollectionViewLayoutAttributes *firstItem = [self layoutAttributesForItemAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:section]];
@@ -59,6 +59,28 @@ NSString *decorationViewOfKind = @"decorationBgView";
 
 - (NSArray *)layoutAttributesForElementsInRect:(CGRect)rect{
     NSMutableArray *attributes = [NSMutableArray arrayWithArray:[super layoutAttributesForElementsInRect:rect]];
+    
+    //使cell紧随前一个
+    for(int i = 1; i < [attributes count]; ++i) {
+        //当前attributes
+        UICollectionViewLayoutAttributes *currentLayoutAttributes = attributes[i];
+        //上一个attributes
+        UICollectionViewLayoutAttributes *prevLayoutAttributes = attributes[i - 1];
+        //间距
+        NSInteger maximumSpacing = 5;
+        //前一个cell的最右边
+        NSInteger prevOrigin = CGRectGetMaxX(prevLayoutAttributes.frame);
+        //需要做偏移
+        if(prevOrigin + maximumSpacing + currentLayoutAttributes.frame.size.width <= self.collectionViewContentSize.width - (self.sectionInset.right+self.sectionInset.left)) {
+            if (currentLayoutAttributes.indexPath.section == prevLayoutAttributes.indexPath.section) {
+                CGRect frame = currentLayoutAttributes.frame;
+                frame.origin.x = prevOrigin + maximumSpacing;
+                currentLayoutAttributes.frame = frame;
+            }
+            
+        }
+    }
+    
     for (UICollectionViewLayoutAttributes *attribute in self.itemAttributes){
         if (!CGRectIntersectsRect(rect, attribute.frame))
             continue;
